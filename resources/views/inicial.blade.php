1fc6a190faf2eab42397b318082e9248729ef00c
Guilherme <guilhermecabrera15@gmail.com>
2023-12-13 15:55:57 -0300

    chore: Criação da tela inicial e das rotas necessarias

---
+++ resources/views/inicial.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/inicial.css') }}">
+    <title>UFSC_Extensao</title>
+</head>
+<body>
+    <h1>Bem vindo ao Sistema</h1>
+    <div class="selecao">
+        <h2>Selecione como deseja acessar o sistema</h2>
+        <a href="login_membro" class="botao">
+            <button>Membro da Sociedade</button>
+        </a>
+        <a href="login_aluno" class="botao">
+            <button>Alunos</button>
+        </a>
+        <a href="login_professor" class="botao">
+            <button>Professores</button>
+        </a>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('extensao')->group(function(){
+    //Rota Inicial de seleção de entrada
+    Route::get('/inicial', function(){
+        return view('inicial');
+    })->name('inicial');
+
+    //Rotas de Login específicas para cada tipo de usuário
+    Route::get('/login_membro', function(){
+        return view('login_membro');
+    })->name('login_membro');
+    Route::get('/login_aluno', function(){
+        return view('login_aluno');
+    })->name('login_aluno');
+    Route::get('/login_professor', function(){
+        return view('login_professor');
+    })->name('login_professor');
 });
