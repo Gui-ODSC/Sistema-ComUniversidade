3dac65c34f21ca93ef866de1b9cd8447a61f29e0
Guilherme <guilhermecabrera15@gmail.com>
2023-12-29 23:38:40 -0300

    chore: adicionado o link para acesso das informações do perfil atraves do icone no canto superior direito da tela

--- resources/views/perfil_membro/perfil.blade.php
+++ resources/views/perfil_membro/perfil.blade.php
@@ -14,7 +14,7 @@
     <header class="barra-navegacao">
         <a href="#" class="btn-abrir" onclick="abrirMenu()">&#9776; Abrir</a>
         <h1>Extensao Universitaria</h1>
-        <a href="#"><img src="{{ asset('img/perfil.png') }}" alt="imagem de perfil do usuario"></a>
+        <a href="{{ route('perfil') }}"><img src="{{ asset('img/perfil.png') }}" alt="imagem de perfil do usuario"></a>
     </header>
     <nav class="menu-navegacao" id="menu_navegacao">
         <a href="#" onclick="fecharMenu()">&times; Fechar</a>
