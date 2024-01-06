79ddc5c0b5dd6d083da71a34f1732879aff6438e
Guilherme <guilhermecabrera15@gmail.com>
2024-01-06 17:50:30 -0300

    chore: ajuste das telas para apenas importar o arquivo de menu no inicio da tela

--- resources/views/demandas_ofertas_membro/demandas_ofertas.blade.php
+++ resources/views/demandas_ofertas_membro/demandas_ofertas.blade.php
@@ -10,20 +10,7 @@
     <title>Demandas e Ofertas</title>
 </head>
 <body> 
-    <header class="barra-navegacao">
-        <a href="#" class="btn-abrir" onclick="abrirMenu()">&#9776; Abrir</a>
-        <h1>Extensao Universitaria</h1>
-        <a href="{{ route('perfil') }}"><img src="{{ asset('img/icones/perfil.png') }}" alt="imagem de perfil do usuario"></a>
-    </header>
-    <nav class="menu-navegacao" id="menu_navegacao">
-        <a href="#" onclick="fecharMenu()">&times; Fechar</a>
-        <a href="{{ route('minhas_ofertas') }}">Minhas Ofertas</a>
-        <a href="{{ route('minhas_demandas') }}">Minhas Demandas</a>
-        <a href="{{ route('demandas_ofertas') }}">Ofertas/Demandas</a>
-        <a href="{{ route('configuracoes') }}">Configurações</a>
-        <a href="{{ route('perfil') }}">Perfil</a>
-        <a href="{{ route('sair') }}">Sair</a>
-    </nav>
+    @include('menu')
     <main class="demandas-ofertas" id="conteudo">
         <h1>Demandas e Ofertas Disponíveis</h1>
         <table class="table table-bordered p-5 table-personalizacao">
