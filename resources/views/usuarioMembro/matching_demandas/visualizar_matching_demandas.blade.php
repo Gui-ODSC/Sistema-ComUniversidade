3627aeb0e2ed5d73c2297a4e96731d4b48a779a5
Guilherme <guilhermecabrera15@gmail.com>
2024-01-30 19:00:50 -0300

    chore: ajuste do arquivo css para a nova tela de Matchings para demandas

--- resources/views/usuarioMembro/matching_demandas/visualizar_matching_demandas.blade.php
+++ resources/views/usuarioMembro/matching_demandas/visualizar_matching_demandas.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/menu_navegacao/menu.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/usuarioMembro/demanda/visualizar_matching_demandas.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/usuarioMembro/matching_demandas/visualizar_matching_demandas.css') }}">
     <script src="{{ asset('js/menu/menu_navegacao.js') }}"></script>
     <script src="{{ asset('js/usuarioMembro/demanda/modal_deletar_demanda.js') }}"></script>
     <title>Matching Demanda</title>
