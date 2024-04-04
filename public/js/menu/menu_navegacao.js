5ac59c6de94b8c1d44c0ecff7e4fc4eb8c9d3d9c
Guilherme <guilhermecabrera15@gmail.com>
2024-04-04 03:01:40 -0300

    chore: Ajustado a performance do menu de selecao

--- public/js/menu/menu_navegacao.js
+++ public/js/menu/menu_navegacao.js
@@ -1,11 +1,11 @@
-
-
 function abrirMenu() {
+    event.preventDefault(); // Impede o comportamento padrão do link
     document.getElementById('menu_navegacao').style.width = '320px';
     document.getElementById('conteudo').style.marginLeft = '320px';
 }
 
 function fecharMenu() {
+    event.preventDefault(); // Impede o comportamento padrão do link
     document.getElementById('menu_navegacao').style.width = '0px';
     document.getElementById('conteudo').style.marginLeft = '0px';
 }
