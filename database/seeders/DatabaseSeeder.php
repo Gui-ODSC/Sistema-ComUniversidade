c7c80c801cd5c0da1a339246bebd71aae965171d
Guilherme <guilhermecabrera15@gmail.com>
2024-05-19 15:37:55 -0300

    chore: Criado os seeders com os dados necessarios para rodar a aplicação

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            AreaConhecimentoSeeder::class,
+            PublicoAlvoSeeder::class,
+            TipoAcaoSeeder::class,
+        ]);
     }
 }
