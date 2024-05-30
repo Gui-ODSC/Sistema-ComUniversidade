cbf5a5c8580cc2ecdfdc4b8acce59d96f2f29ac9
Guilherme <guilhermecabrera15@gmail.com>
2024-05-30 03:51:03 -0300

    chore: Configuracao de e-mail

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'SES'),
 
     /*
     |--------------------------------------------------------------------------
@@ -100,8 +100,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'comuniversidade@oliveira.systems'),
+        'name' => env('MAIL_FROM_NAME', 'Comuniversidade'),
     ],
 
     /*
