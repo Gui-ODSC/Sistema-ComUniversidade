5d925d058eb72be4604c9277d53f0985eb90e757
Guilherme <guilhermecabrera15@gmail.com>
2024-06-03 17:47:13 -0300

    chore: Ajuste do olhinho para todos os perfis

--- resources/views/usuarioMembro/cadastro/cadastro_membro.blade.php
+++ resources/views/usuarioMembro/cadastro/cadastro_membro.blade.php
@@ -151,10 +151,10 @@
                                 <span>Senha *</span>
                             </label>
                             <!-- Ícone informativo -->
-                            <span class="info-icon" onclick="showPasswordRules()" style="position: absolute; top: 50%; left: 465px;; transform: translateY(-50%); cursor: pointer;">
+                            <span class="info-icon" onclick="showPasswordRules()" style="position: absolute; top: 50%; left: 95%; transform: translateY(-50%); cursor: pointer;">
                                 <img src="{{ asset('img/cadastro_usuarios/icone_info_senha.png') }}" alt="Ícone informativo" style="width: 20px;">
                             </span>
-                            <span class="toggle-password" onclick="togglePassword()" style="position: absolute; top: 50%; left: 430px; transform: translateY(-50%); cursor: pointer; display: none"><img src="{{ asset('img/usuarioMembro/cadastrar_demandas/olho_desmarcado.png')}}" alt="" style="width: 25px"></span>
+                            <span class="toggle-password" onclick="togglePassword()" style="position: absolute; top: 50%; left: 89%; transform: translateY(-50%); cursor: pointer; display: none"><img src="{{ asset('img/usuarioMembro/cadastrar_demandas/olho_desmarcado.png')}}" alt="" style="width: 25px"></span>
                         </div>
                     @else
                         <div style="position: relative;">
@@ -162,9 +162,9 @@
                             <label for="password">
                                 <span>Senha *</span>
                             </label>
-                            <span class="toggle-password" onclick="togglePassword()" style="position: absolute; top: 50%; left: 90%; transform: translateY(-50%); cursor: pointer; display: none"><img src="{{ asset('img/usuarioMembro/cadastrar_demandas/olho_desmarcado.png')}}" alt="" style="width: 25px"></span>
+                            <span class="toggle-password" onclick="togglePassword()" style="position: absolute; top: 50%; left: 95%; transform: translateY(-50%); cursor: pointer; display: none"><img src="{{ asset('img/usuarioMembro/cadastrar_demandas/olho_desmarcado.png')}}" alt="" style="width: 25px"></span>
                             <!-- Ícone informativo -->
-                            <span class="info-icon" onclick="showPasswordRules()" style="position: absolute; top: 50%; left: 95%; transform: translateY(-50%); cursor: pointer;">
+                            <span class="info-icon" onclick="showPasswordRules()" style="position: absolute; top: 50%; left: 89%; transform: translateY(-50%); cursor: pointer;">
                                 <img src="{{ asset('img/cadastro_usuarios/icone_info_senha.png') }}" alt="Ícone informativo" style="width: 20px;">
                             </span>
                         </div>
